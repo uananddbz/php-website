1d08a893748a836b4de86ae339662fd5a4638c8e
uananddbz <uananddbz@gmail.com>
2013-08-03 18:23:32 +0530

    Create headtag.php

---
+++ headtag.php
@@ -0,0 +1,9 @@
+<script src="js/jquery.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+<link href="css/my.css" rel="stylesheet">
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.validate.js"></script>
+<script src="js/my.js"></script>
