5ba3d74d7d2147e20caf8d2600b29615c429a4c2
uananddbz <uananddbz@gmail.com>
2013-08-03 18:43:19 +0530

    Create login.php

---
+++ website/login.php
@@ -0,0 +1,78 @@
+<?php $title='Login'; ?>
+<?php require_once('header.php'); ?>
+<?php
+$tbl_name='members';
+
+if (isset($_GET['link'])){$link=$_GET['link'];}
+if (isset($_POST['redirect'])){$redirect=$_POST['redirect'];}
+
+if (isset($_SESSION['username'])) {
+info('already login.');
+}
+else {
+if (isset($_POST['username'])) {
+// username and password sent from form 
+$myusername=$_POST['username']; 
+$mypassword=$_POST['password']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$myusername = stripslashes($myusername);
+$mypassword = stripslashes($mypassword);
+$myusername = mysql_real_escape_string($myusername);
+$mypassword = mysql_real_escape_string($mypassword);
+$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+$result=mysql_query($sql);
+    $count = mysql_num_rows($result);
+  if ($count) {
+while($row = mysql_fetch_array($result))
+  {
+		$_SESSION['username'] = $myusername; //username
+		$_SESSION['id']=$row['id']; //id of user
+		$_SESSION['name']=$row['name']; //name of user
+		$_SESSION['email']=$row['email']; //email of user
+		$_SESSION['country']=$row['country']; //country of user
+		$_SESSION['gender']=$row['gender']; //gender of user
+		$_SESSION['dob']=$row['dob']; //user's date of birth
+		if (isset($redirect)){
+		header("location:$redirect");
+		}
+		else
+		header("location:index.php");
+  }
+  }
+  
+  else
+  
+error('Wrong Username or Password');
+}
+
+}
+?>
+<form class="form-horizontal" action="<?=$_SERVER['PHP_SELF']?>" method="post">
+  <div class="control-group">
+    <label class="control-label" for="username">Username</label>
+    <div class="controls">
+	
+      <?php
+	if (isset($link))
+	{
+	echo '<input name="redirect" type="hidden" value="'.$link.'" />';
+	}
+	?>
+	
+      <input type="text" name="username" id="username" placeholder="Username">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputPassword">Password</label>
+    <div class="controls">
+      <input type="password" name="password" id="inputPassword" placeholder="Password">
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </div>
+  </div>
+</form>
+<?php require_once('footer.php'); ?>
