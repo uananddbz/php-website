2cfa720c3de027678fe5be6dd8c45a1a595a6548
uananddbz <uananddbz@gmail.com>
2013-08-03 18:44:23 +0530

    Create contacts.php

---
+++ website/contacts.php
@@ -0,0 +1,24 @@
+<?php $title='Contacts';?>
+<?php require_once('header.php'); ?>
+<?php require_once('auth.php'); ?>
+<div class="container-fluid">
+
+  <div class="page-header">
+    <h1>Contacts <small>to contact</small></h1>
+  </div>
+  
+  <div class="ajax-load-bs">
+  <ul class="nav nav-my nav-tabs">
+    <li class="active"> <a title="HOME" href="view_contacts.php"><i class="icon-home"></i></a> </li>
+  <li><a title="ADD" href="add_contacts.php"><i class="icon-plus"></i></a></li>
+    <li><a title="DELETE" href="edit_contacts.php"><i class="icon-trash"></i></a></li>
+  </ul>
+  <div class="bod" style="border-bottom:2px solid #ddd;">
+  <div class="con container-fluid">
+
+</div></div>
+
+</div>
+
+</div>
+<?php require_once('footer.php'); ?>
