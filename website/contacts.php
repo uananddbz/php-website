a372fcab18b6f33731c4f141f98b5150f52a18d7
uananddbz <uananddbz@gmail.com>
2013-08-05 18:38:05 +0530

    Update contacts.php

--- website/contacts.php
+++ website/contacts.php
@@ -1,22 +1,24 @@
 <?php $title='Contacts';?>
 <?php require_once('header.php'); ?>
 <?php require_once('auth.php'); ?>
+
+
+
 <div class="container-fluid">
 
   <div class="page-header">
     <h1>Contacts <small>to contact</small></h1>
   </div>
   
-  <div class="ajax-load-bs">
-  <ul class="nav nav-my nav-tabs">
+  <ul id="loader" class="nav nav-my nav-tabs">
     <li class="active"> <a title="HOME" href="view_contacts.php"><i class="icon-home"></i></a> </li>
   <li><a title="ADD" href="add_contacts.php"><i class="icon-plus"></i></a></li>
     <li><a title="DELETE" href="edit_contacts.php"><i class="icon-trash"></i></a></li>
   </ul>
-  <div class="bod" style="border-bottom:2px solid #ddd;">
-  <div class="con container-fluid">
+  
+  <div id="target" class="container-fluid" style="border-bottom:1px solid #ddd;">
 
-</div></div>
+</div>
 
 </div>
 
