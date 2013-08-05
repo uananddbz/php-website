4ec76a1a08a519c84bacefc768eaf2657ce6511a
uananddbz <uananddbz@gmail.com>
2013-08-05 18:35:07 +0530

    Update my.js

--- website/js/my.js
+++ website/js/my.js
@@ -1,104 +1,14 @@
  
   $(document).ready(function(){
-  						 
+
+//loading animation
+
+
+
 // for tooltip						 
-  function tol()
-{
   $("[title]").mouseover(function(){
 $(this).tooltip('show');
   });
-}
-
-tol();
-
-//ajax load navbar and body
-
-$(".ajax-load").each(function(){
-nav=$(this).children(".nav");
-but=nav.children("a");
-bod=$(this).children(".bod");;
-con=bod.children(".con");
-
-f=nav.children("a.active").attr("href");
-     $(con).load(f, function(response, status, xhr){
-    if(status == "error") {
-    $(this).html(xhr.status + " " + xhr.statusText);
-  }
-  
-});
-
-
-$(but).click(function () {
-el=$(this);
-link=$(this).attr("href");
-bod=el.parent(".nav").parent(".ajax-load").children(".bod");;
-con=bod.children(".con");
-
-if (el.hasClass("active")) {}
-
-else {
- $(bod).slideUp("fast", function(){
- el.parent(".nav").children(".active").removeClass("active");
-el.addClass("active");
-  $(con).load(link, function(response, status, xhr){
-    if(status == "error") {
-    $(con).html(xhr.status + " " + xhr.statusText);
-  }
-  $(bod).slideDown("fast",function(){
-    $(con).fadeTo("normal",1);
-  });
-
-});
-});
-
-}
-return false;
-});
-
-});
-
-//ajax load navbar for bootstrap
-$(".ajax-load-bs").each(function(){
-nav=$(this).children(".nav");
-but=nav.children("li").children("a");
-bod=$(this).children(".bod");
-con=bod.children(".con");
-
-f=nav.children("li.active").children("a").attr("href");
-     $(con).load(f, function(response, status, xhr){
-    if(status == "error") {
-    $(this).html(xhr.status + " " + xhr.statusText);
-  }
-  
-});
-
-
-$(but).click(function () {
-el=$(this);
-link=$(this).attr("href");
-bod=el.parent("li").parent(".nav").parent(".ajax-load-bs").children(".bod");
-con=bod.children(".con");
-
-if (el.parent("li").hasClass("active")) {}
-
-else {
- $(bod).slideUp("fast", function(){
- el.parent("li").parent(".nav").children(".active").removeClass("active");
-el.parent("li").addClass("active");
-  $(con).load(link, function(response, status, xhr){
-    if(status == "error") {
-    $(con).html(xhr.status + " " + xhr.statusText);
-  }
-  $(bod).slideDown("fast");
-
-});
-});
-
-}
-return false;
-});
-
-});
 
 
 
