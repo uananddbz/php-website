70da8ffc53f86dc04e9b4d2a35c8251fc63c3ee8
uananddbz <uananddbz@gmail.com>
2013-08-14 19:25:36 +0530

    Update headtag.php

--- website/headtag.php
+++ website/headtag.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+<link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css" />
 <link href="css/my.css" rel="stylesheet">
 <script src="js/jquery-ui.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
@@ -9,3 +10,4 @@
 <script src="js/my.js"></script>
 <script src="js/ajax.js"></script>
 <script src="js/nav.js"></script>
+<script src="js/hs.js"></script>
