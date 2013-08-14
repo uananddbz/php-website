0e3a7de90b262d51874f18f2f97053cd6080f47d
uananddbz <uananddbz@gmail.com>
2013-08-14 19:32:47 +0530

    Update footer.php

--- website/footer.php
+++ website/footer.php
@@ -1,2 +1,10 @@
+
+</div>
+
+<footer class="site-footer">
+<div class="container">
+        <p class="muted credit">view this on - <a href="https://github.com/uananddbz/php-website">Github</a><a href="guestbook.php" class="btn pull-right">Guestbook</a></p>
+	        </div>
+</footer>
 </body>
 </html>
