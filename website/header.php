3b648d1248ac359aae2e001ea2adfdd2937f6596
uananddbz <uananddbz@gmail.com>
2013-08-14 19:27:58 +0530

    Update header.php

--- website/header.php
+++ website/header.php
@@ -8,42 +8,13 @@
 uananddbz</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php require_once('headtag.php'); ?>
-<script type="text/javascript">
-$(document).ready(function(){
-  $("#loader").ajaxl("#target","<b>Loading...</b>");
-});
-</script>
 </head>
 <body>
+<div class="page-wrap">
 <div class="navbar">
-  <div class="navbar-inner">
-    <div class="container"> <a class="brand" href="index.php">UANANDDBZ</a>
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
-      <div class="nav-collapse collapse">
-        <?php if(isset($nav))echo $nav; ?>
-        <ul class="nav pull-right">
-          <?php if(isset($_SESSION['username'])){
-echo '
-	<li><a href="contacts.php">Contacts</a></li>
-	  	    <li class="divider-vertical"></li>
-<li class="dropdown">
-  <a href="" class="dropdown-toggle" data-toggle="dropdown"><i class="caret"></i></a>
-  <ul class="dropdown-menu"><li class="nav-header"><strong>'.$_SESSION['username'].'</strong></li>
-  <li class="divider"></li>
-  <li><a href="profile.php"><i class="icon-user"></i> profile</a></li><li><a href="'.$url.'?logout=1"><i class="icon-off"></i> Logout</a></li></ul></li>';
-}
-elseif (isset($_GET['link'])) {
-echo '<li class="active"><a><b>Please enter your username and password.</b></a></li>';
-}
-else {
-echo '<li><a href="login.php?link='.$url.'" data-toggle="modal">Login</a></li>';
-} 
-
-?>
-        </ul>
-      </div>
-    </div>
+  <div class="navbar-inner  my-nav">
+ <a class="brand" href="index.php">UANANDDBZ</a>
+        <?php if(isset($nav))echo $nav; else {require_once('nav.php');} ?>
   </div>
 </div>
 <?php if (isset($check)) echo $check; ?>
