f98fc55c253f43fddb14f97f2bd71dc84bc3fd24
utkarsh anand <uananddbz@gmail.com>
2013-08-17 19:53:48 +0530

    Create 404.php

---
+++ website/404.php
@@ -0,0 +1,15 @@
+<?php $title='HOME';?>
+<?php require_once('header.php'); ?>
+
+<div class="hero-unit" style="background:#fdd;">
+  <h1>404 error</h1>
+  <p>
+  Oops! shit is not here
+    <a href="index.php" class="btn btn-primary btn-large">
+     start from home
+    </a>
+  </p>
+</div>
+
+
+<?php require_once('footer.php'); ?>
