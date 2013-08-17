452065c751a2f12a2ee0e6a6f3c5b904b9df5885
utkarsh anand <uananddbz@gmail.com>
2013-08-17 19:40:13 +0530

    Update headtag.php

--- website/headtag.php
+++ website/headtag.php
@@ -1,7 +1,7 @@
 <script src="js/jquery.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-<link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
 <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css" />
 <link href="css/my.css" rel="stylesheet">
 <script src="js/jquery-ui.min.js"></script>
@@ -9,5 +9,4 @@
 <script src="js/jquery.validate.js"></script>
 <script src="js/my.js"></script>
 <script src="js/ajax.js"></script>
-<script src="js/tool.js"></script>
 <script src="js/hs.js"></script>
