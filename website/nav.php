ecd2852e9680cb020c01f593cd01b8b3f1ca54af
utkarsh anand <uananddbz@gmail.com>
2013-08-17 19:38:35 +0530

    Update nav.php

--- website/nav.php
+++ website/nav.php
@@ -1,14 +1,22 @@
 		  <?php if(isset($_SESSION['username'])){
+$id=$_SESSION["id"];
+$totalc=mysql_num_rows(mysql_query("SELECT * FROM contacts WHERE id='$id'"));
 echo '
 
+  <ul class="nav">
+  <li>
+            <a href="#" data-placement="bottom" class="contact-nav" data-toggle="popover">Contacts <span class="badge badge-inverse">'.$totalc.'</span></a></li>
+  <li><a data-placement="bottom" class="profile-nav" data-toggle="popover" href="#"><i class="icon-user icon-white"></i></a></li></ul>
   <ul class="nav pull-right">
-  <li><a data-placement="bottom" title="'.$_SESSION['username'].'" href="profile.php"><i class="icon-user icon-white"></i></a></li><li><a href="'.$url.'?logout=1" title="logout" data-placement="bottom"><i class="icon-off icon-white"></i></a></li></ul>';
+  <li><a href="'.$url.'?logout=1" title="logout" data-placement="bottom"><i class="icon-off icon-white"></i></a></li></ul>';
 }
 else {
 echo '
 <script>
 $(document).ready(function(){
-$(".show-username").hs("  <button class=btn type=submit>Login/Register</button>");
+$(".show-username").hs(function() {
+$(this).replaceWith("<button class=btn type=submit>Login/Register</button>");
+});
 });
 </script>
 <form class="navbar-form pull-right" action="check.php" method="GET">
