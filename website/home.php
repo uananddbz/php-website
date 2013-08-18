7e7710694823c76f904d91851bf2bcd1704d04db
utkarsh anand <uananddbz@gmail.com>
2013-08-18 18:26:44 +0530

    Update home.php

--- website/home.php
+++ website/home.php
@@ -1,19 +1,11 @@
 <?php $title='HOME';?>
 <?php require_once('header.php'); ?>
 
-<?php require_once('auth.php'); 
-$tbl_name='contacts';
-$id=$_SESSION["id"];
-
-$sql="SELECT * FROM $tbl_name WHERE id='$id' ORDER BY name";
-$result=mysql_query($sql);
-if (mysql_num_rows($result)!="0")
-    $count = mysql_num_rows($result);
-?>
+<?php require_once('auth.php'); ?>
 
 <div class="container">
 	  
-	  <a class="btn btn-block btn-large" href="contacts.php">Contacts <span class="badge badge-inverse"><?php if (isset($count)) {echo $count;}?></span></a>
+	  <a class="btn btn-block btn-large" href="edit-contacts.php">Contacts <span class="badge badge-inverse"><?=$count_contact;?></span></a>
 
 </div>
 
