271e5cb6ce5925c6d026ce042dcca6bd8519aba2
utkarsh anand <uananddbz@gmail.com>
2013-08-18 18:26:08 +0530

    Update index.php

--- website/index.php
+++ website/index.php
@@ -1,11 +1,28 @@
-<?php require_once("header.php"); ?>
-
-<div class="container">
-	  <?php
-	  if (isset($_SESSION["id"])) {
+<?php require_once('var.php'); ?>
+<?php require_once('config.php'); ?>
+<?php
+	  if (isset($_SESSION["username"])) {
 header("location:home.php");
 	  }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php require_once('headtag.php'); ?>
+</head>
+<body>
+<div class="page-wrap">
+
+
+<div class="container">
+<div class="page-header">
+  <h1 style="color:#4096ee">UANANDDBZ <small>make simple live better</small></h1>
+</div>
+
+    <?php if(isset($nav))echo $nav; else {require_once('nav.php');} ?>
+
     <div class="hero-unit bg-none">
 <h1>Features</h1>
 	  <ul>
@@ -20,5 +37,4 @@ header("location:home.php");
 
 
  </div>
-
 <?php require_once("footer.php"); ?>
