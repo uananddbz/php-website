d8cffbaeb71479a4eefc7382f10e7301ac22768f
utkarsh anand <uananddbz@gmail.com>
2013-08-18 18:09:43 +0530

    Update nav.php

--- website/nav.php
+++ website/nav.php
@@ -1,16 +1,20 @@
 		  <?php if(isset($_SESSION['username'])){
+$tbl_name='contacts';
 $id=$_SESSION["id"];
-$totalc=mysql_num_rows(mysql_query("SELECT * FROM contacts WHERE id='$id'"));
+
+$sql="SELECT * FROM $tbl_name WHERE id='$id' ORDER BY fname";
+$result=mysql_query($sql);
+    $count_contact = mysql_num_rows($result);
 echo '
 
   <ul class="nav">
   <li>
-            <a href="#" data-placement="bottom" class="contact-nav" data-toggle="popover">Contacts <span class="badge badge-inverse">'.$totalc.'</span></a></li>
+            <a href="#" data-placement="bottom" class="contact-nav" data-toggle="popover">Contacts <span class="badge badge-inverse">'.$count_contact.'</span></a></li>
   <li><a data-placement="bottom" class="profile-nav" data-toggle="popover" href="#"><i class="icon-user icon-white"></i></a></li></ul>
   <ul class="nav pull-right">
   <li><a href="'.$url.'?logout=1" title="logout" data-placement="bottom"><i class="icon-off icon-white"></i></a></li></ul>';
 }
-else {
+else 
 echo '
 <script>
 $(document).ready(function(){
@@ -27,7 +31,7 @@ $(this).replaceWith("<button class=btn type=submit>Login/Register</button>");
   <button data="#username" class="btn show-username">Login/Register</button></div>
 </form>
 ';
-} 
+
 
 ?>
 
