22f04b3114bc8a2d1211f37e2eda41ef13ee0698
utkarsh anand <uananddbz@gmail.com>
2013-08-22 18:49:40 +0530

    Update register.php

--- website/register.php
+++ website/register.php
@@ -2,6 +2,13 @@
 <?php
 $nav='<ul class="nav pull-right"><li class="active"><a>Please fill all details below</a></li></ul>';
  require_once("header.php"); ?>
+<script type="text/javascript">
+  $(function() {
+    $('#datetimepicker').datetimepicker({
+pickTime: false
+    });
+  });
+</script>
 
 <?php
 if (isset($_GET['link'])){$link=$_GET['link'];}
@@ -15,9 +22,7 @@ $name=$_POST['name'];
 $email=$_POST['email'];
 $country=$_POST['country'];
 $gender=$_POST['gender'];
-$d=$_POST['d'];
-$m=$_POST['m'];
-$y=$_POST['y'];
+$dob=$_POST['dob'];
 $tbl="members";
 
 //check username and other fields before register
@@ -25,18 +30,20 @@ $sql="SELECT * FROM $tbl WHERE username='$username'";
 $result=mysql_query($sql);
     $row = mysql_num_rows($result);
 
-if($row || empty($username) || empty($password) || empty($name) || empty($email) || empty($country) || $gender=="gender" || $d=="date" || $m=="month" || $y=="year"  ){
+if($row || empty($username) || empty($password)){
 
 if($row){
 error("<strong>Username</strong> already exist.");
 }
-if($d=="date" || $m=="month" || $y=="year"){
-error("check your <strong>date of birth</strong>.");
+if(empty($username)){
+error("<strong>Username</strong> can't empty.");
+}
+if(empty($password)){
+error("<strong>password</strong> can't empty.");
 }
 
 }
 else {
-$dob=$y.'-'.$m.'-'.$d;
 insert ('username, password, name, email, country ,gender ,dob',"'$username' , '$password' , '$name', '$email', '$country' ,'$gender' ,'$dob'",$tbl);
 		if (isset($redirect)){
 header("location:login.php?link=".$redirect."&username=".$username);
@@ -67,9 +74,7 @@ header("location:login.php?username=".$username);
 	}
 	else {
 echo  '
-      <input type="text" name="username" value="';
-	  if(isset($_POST['username'])){echo $_POST['username'];}
-	  echo '" placeholder="Username" required="required">
+      <input type="text" name="username" value="" placeholder="Username" required>
 ';
 }
 
@@ -77,127 +82,66 @@ echo  '
 	  <span class="help-inline"></span>
 	  </div>
   </div>
-    <div class="control-group">
-    <label class="control-label" for="name">Name</label>
-    <div class="controls">
-      <input type="text" value="<?php if(isset($_POST['name'])){echo $_POST['name'];} ?>" name="name" id="name" placeholder="Name" required> 
-	  <span class="help-inline"></span>
-    </div>
-  </div>
-    <div class="control-group">
-    <label class="control-label" for="email">Email</label>
-    <div class="controls">
-      <input type="email" name="email" value="<?php if(isset($_POST['email'])){echo $_POST['email'];} ?>" id="email" placeholder="email" required>
-	  <span class="help-inline"></span>
-    </div>
-  </div>
-    <div class="control-group">
-    <label class="control-label" for="country">Country</label>
-    <div class="controls">
-      <input type="text" name="country" value="<?php if(isset($_POST['country'])){echo $_POST['country'];} ?>" id="country" placeholder="Country" required> 
-	  <span class="help-inline"></span>
-    </div>
-  </div>
-    <div class="control-group">
-    <label class="control-label" for="gender">Gender</label>
-    <div class="controls">
-
-			<label  class="radio inline" for="gender_male">
-				<input type="radio" checked="checked" id="gender_male" value="male" name="gender">
-				Male
-			</label>
-			<label  class="radio inline" for="gender_female">
-				<input type="radio" id="gender_female" value="female" name="gender" required="required">
-				Female
-			</label>
-
-    </div>
-  </div>
-    <div class="control-group">
-    <label class="control-label" for="dob" placeholder="yyyy-mm-dd">Date of birth</label>
-    <div class="controls">
-<select  class="span2" name="d">
-<option selected="selected">date</option>
-<?php
-for ($i=1; $i<=9; $i++)
-  {
-  if (isset($d)) {
-  if ($d==$i) {
-  echo "<option selected='selected'>$i</option>";}
-    else
-  echo"<option>$i</option>";
-  }
-  else
-  echo"<option>$i</option>";
-  }
-  for ($i=10; $i<=31; $i++)
-  {
-  if (isset($d)) {
-  if ($d==$i) {
-  echo "<option selected='selected'>$i</option>";}
-    else
-  echo"<option>$i</option>";
-  }
-  else
-  echo"<option>$i</option>";
-  }
-?>
-
-</select>
-<select  class="span2" name="m">
-<option selected="selected">month</option>
-<?php
-for ($i=1; $i<=9; $i++)
-  {
-  if (isset($m)) {
-  if ($m==$i) {
-  echo "<option selected='selected'>$i</option>";}
-    else
-  echo"<option>$i</option>";
   
-  }
-  else
-  echo"<option>$i</option>";
-  }
-  for ($i=10; $i<=12; $i++)
-  {
-  if (isset($m)) {
-  if ($m==$i) {
-  echo "<option selected='selected'>$i</option>";}
-    else
-  echo"<option>$i</option>";
-  }
-  else
-  echo"<option>$i</option>";
-  }
-?>
-
-</select>
-<select class="span2" name="y">
-<option selected="selected">year</option>
-<?php
-for ($i=1980; $i<=2009; $i++)
-  {
-  if (isset($y)) {
-  if ($y==$i) {
-  echo "<option selected='selected'>$i</option>";}
-    else
-  echo"<option>$i</option>";
-  }
-  else
-  echo"<option>$i</option>";
-  }
-?>
-</select>
-    </div>
-  </div>
-  <div class="control-group">
+    <div class="control-group">
     <label class="control-label" for="inputPassword">Password</label>
     <div class="controls">
       <input type="password" name="password" id="inputPassword" placeholder="Password" required>
 	  <span class="help-inline"></span>
     </div>
   </div>
+  
+    <div class="control-group">
+    <label class="control-label" for="name">Name</label>
+    <div class="controls">
+      <input type="text" value="" name="name" id="name" placeholder="Name"> 
+	  <span class="help-inline"></span>
+    </div>
+  </div>
+  
+   <div class="control-group">
+    <label class="control-label" for="dob" placeholder="yyyy-mm-dd">Date of birth</label>
+    <div class="controls">
+<div id="datetimepicker" class="input-append">
+    <input id="dob" class="input-small" placeholder="YYYY-MM-DD" name="dob" data-format="yyyy-MM-dd" type="text"></input>
+    <span class="add-on">
+      <i data-time-icon="icon-time" data-date-icon="icon-calendar">
+      </i>
+    </span>
+  </div>
+    </div>
+  </div>
+  
+    <div class="control-group">
+    <label class="control-label" for="email">Email</label>
+    <div class="controls">
+      <input type="email" name="email" value="" id="email" placeholder="email">
+	  <span class="help-inline"></span>
+    </div>
+  </div>
+    <div class="control-group">
+    <label class="control-label" for="country">Country</label>
+    <div class="controls">
+      <input type="text" name="country" value="" id="country" placeholder="Country"> 
+	  <span class="help-inline"></span>
+    </div>
+  </div>
+    <div class="control-group">
+    <label class="control-label" for="gender">Gender</label>
+    <div class="controls">
+<input type="hidden" checked="checked" value="" name="gender">
+			<label  class="radio inline" for="gender_male">
+				<input type="radio" id="gender_male" value="male" name="gender">
+				Male
+			</label>
+			<label  class="radio inline" for="gender_female">
+				<input type="radio" id="gender_female" value="female" name="gender">
+				Female
+			</label>
+
+    </div>
+  </div>
+   
 <div class="control-group">
 <div class="controls">
       <button type="submit" class="btn btn-primary">Register</button>
