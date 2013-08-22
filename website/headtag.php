84f1bd6632888a1bb0f396fdc25eb63474f78ff8
utkarsh anand <uananddbz@gmail.com>
2013-08-22 18:56:50 +0530

    Update headtag.php

--- website/headtag.php
+++ website/headtag.php
@@ -1,10 +1,11 @@
 <script src="js/jquery.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="css/bootstrap.min.css" rel="stylesheet" media="screen" />
-<link href="css/bootstrap-responsive.min.css" rel="stylesheet" />
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
 <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css" />
-<link href="css/my.css" rel="stylesheet" />
-<link href="css/bootstrap-editable.css" rel="stylesheet" />
+<link href="css/my.css" rel="stylesheet">
+<link href="css/bootstrap-editable.css" rel="stylesheet">
+<link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet">
 <script src="js/jquery-ui.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/jquery.validate.js"></script>
@@ -12,3 +13,4 @@
 <script src="js/hs.js"></script>
 <script src="js/jquery.bootstrap-growl.min.js"></script>
 <script src="js/bootstrap-editable.min.js"></script>
+<script src="js/bootstrap-datetimepicker.min.js"></script>
