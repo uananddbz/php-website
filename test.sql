7563b23458d7fd6f93756ddfad00d92714cf64c9
utkarsh anand <uananddbz@gmail.com>
2013-08-22 18:40:05 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2013 at 04:00 PM
+-- Generation Time: Aug 22, 2013 at 03:02 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -20,29 +20,67 @@ SET time_zone = "+00:00";
 -- Database: `test`
 --
 
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `test_multi_sets`()
+    DETERMINISTIC
+begin
+        select user() as first_col;
+        select user() as first_col, now() as second_col;
+        select user() as first_col, now() as second_col, now() as third_col;
+        end$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contacts`
+-- Table structure for table `guestbook`
 --
 
-CREATE TABLE IF NOT EXISTS `contacts` (
+CREATE TABLE IF NOT EXISTS `guestbook` (
+  `index` int(4) NOT NULL AUTO_INCREMENT,
+  `id` varchar(20) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `msg` varchar(200) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `username` varchar(65) NOT NULL DEFAULT '',
+  `password` varchar(65) NOT NULL DEFAULT '',
+  `name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `country` varchar(10) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE IF NOT EXISTS `notes` (
   `index` int(4) NOT NULL AUTO_INCREMENT,
   `id` varchar(10) NOT NULL,
-  `fname` varchar(20) NOT NULL,
-  `lname` varchar(20) NOT NULL,
-  `number` varchar(20) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `content` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`index`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `contacts`
---
-
-INSERT INTO `contacts` (`index`, `id`, `fname`, `lname`, `number`) VALUES
-(1, '1', 'test', 'name', '0123456789'),
-(2, '1', 'fname', 'lname', '+0123456789'),
-(3, '1', 'first', 'last', 'number');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
