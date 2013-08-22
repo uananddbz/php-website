0063e943eeddeab411167b5e982ea39cda57b12a
utkarsh anand <uananddbz@gmail.com>
2013-08-22 18:45:14 +0530

    Update hs.js

--- website/js/hs.js
+++ website/js/hs.js
@@ -3,17 +3,27 @@
     $.fn.hs = function(callback) {
 
 this.each(function(){
-				     data=$(this).attr("data");
+				     data=$(this).attr("data-target");
 					   cont=$(data);
 					   cont.hide();
   $(this).click(function(){
 
 
-data=$(this).attr("data");
+data=$(this).attr("data-target");
   cont=$(data);
   el=$(this);
-  $(cont).toggle("fast");
-  if (callback != 'undefined') {
+  effect=$(this).attr("data-effect");
+  
+if (effect==undefined || effect=='') {
+	$(cont).toggle("slow");
+
+}
+
+else {
+$(cont).toggle(effect,"slow");	
+}
+  
+  if ($.isFunction(callback)) {
           callback.call(this);	 
   }
 
